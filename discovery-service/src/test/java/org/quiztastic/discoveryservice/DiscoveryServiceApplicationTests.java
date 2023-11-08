ef594f157408302d5904adc22983de32697ba949
Ena <enachealexandru00@yahoo.com>
2023-11-08 23:44:56 +0200

    Add eureka discovery service

---
+++ discovery-service/src/test/java/org/quiztastic/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quiztastic.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
--- question-service/src/main/java/org/quiztastic/questionservice/service/QuestionService.java
+++ question-service/src/main/java/org/quiztastic/questionservice/service/QuestionService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.quiztastic.questionservice.model.Question;
 import org.quiztastic.questionservice.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -12,6 +13,8 @@ import java.util.List;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+
+    private final WebClient.Builder webClientBuilder;
 
     public List<Question> getAllQuestions() {
         return questionRepository.findAllByOrderByCreationDesc();
